371efca4e320f7a6908b7c1d78b86cbafce2fd9b
malamq98@gmail.com <malamq98@gmail.com>
2023-05-07 09:53:30 -0500

    adding codes

---
+++ src/main/java/objects/HomePage.java
@@ -0,0 +1,32 @@
+package objects;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+	
+	public HomePage(WebDriver driver) {
+		initElements(driver, this);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/utils/Key.java
@@ -0,0 +1,5 @@
+package utils;
+
+public enum Key {
+	browser, pageLoad, implicitLoad, explicitWait, url
+}
